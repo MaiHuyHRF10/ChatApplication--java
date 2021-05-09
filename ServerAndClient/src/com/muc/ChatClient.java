77676aecb73634a077a8e9d7e569cfd018941555
MaiHuyHRF10 <trinhmaihuyak53@gmail.com>
2021-05-09 13:50:56 +0700

    ver1

---
+++ ServerAndClient/src/com/muc/ChatClient.java
@@ -0,0 +1,143 @@
+package com.muc;
+
+
+import sun.rmi.runtime.Log;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ChatClient {
+    private final String serverName;
+    private final int serverPort;
+    private Socket socket;
+    private InputStream serverIn;
+    private OutputStream serverOut;
+    private BufferedReader bufferedIn;
+
+    private UserStatusListener userStatusListener = new UserStatusListener() {
+        @Override
+        public void online(String login) {
+
+        }
+
+        @Override
+        public void offline(String login) {
+
+        }
+    };
+    private MessageListener messageListener = new MessageListener() {
+        @Override
+        public void onMessage(String fromLogin, String msgBody) {
+
+        }
+    };
+
+    public ChatClient(String serverName, int serverPort) {
+        this.serverName = serverName;
+        this.serverPort = serverPort;
+    }
+
+
+    public void msg(String sendTo, String msgBody) throws IOException {
+        String cmd = "msg " + sendTo + " " + msgBody + "\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    public boolean login(String login, String password) throws IOException {
+        String cmd = "login " + login + " " + password + "\n";
+        serverOut.write(cmd.getBytes());
+
+        String response = bufferedIn.readLine();
+        System.out.println("Response Line:" + response);
+
+        if ("ok login".equalsIgnoreCase(response)) {
+            startMessageReader();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logoff() throws IOException {
+        String cmd = "logoff\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    private void startMessageReader() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                readMessageLoop();
+            }
+        };
+        t.start();
+    }
+
+    private void readMessageLoop() {
+        try {
+            String line;
+            while ((line = bufferedIn.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                if (tokens != null && tokens.length > 0) {
+                    String cmd = tokens[0];
+                    if ("online".equalsIgnoreCase(cmd)) {
+                        handleOnline(tokens);
+                    } else if ("offline".equalsIgnoreCase(cmd)) {
+                        handleOffline(tokens);
+                    } else if ("msg".equalsIgnoreCase(cmd)) {
+                        String[] tokensMsg = line.split(" ", 3);
+                        handleMessage(tokensMsg);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleMessage(String[] tokensMsg) {
+        String login = tokensMsg[1];
+        String msgBody = tokensMsg[2];
+
+        messageListener.onMessage(login, msgBody);
+    }
+
+    private void handleOffline(String[] tokens) {
+        String login = tokens[1];
+        userStatusListener.offline(login);
+    }
+
+    private void handleOnline(String[] tokens) {
+        String login = tokens[1];
+
+        userStatusListener.online(login);
+    }
+
+    public boolean connect() {
+        try {
+            this.socket = new Socket(serverName, serverPort);
+            System.out.println("Client port is " + socket.getLocalPort());
+            this.serverOut = socket.getOutputStream();
+            this.serverIn = socket.getInputStream();
+            this.bufferedIn = new BufferedReader(new InputStreamReader(serverIn));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void addUserStatusListener(UserStatusListener listener) {
+        userStatusListener = listener;
+    }
+
+    public void addMessageListener(MessageListener listener) {
+        messageListener = listener;
+    }
+
+}
